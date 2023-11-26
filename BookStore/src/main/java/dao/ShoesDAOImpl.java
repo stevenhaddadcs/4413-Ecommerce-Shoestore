da9f088e962084aef86b72e2667b79c8ef1729fa
haddad-steven <stevenhaddadcs@gmail.com>
2023-11-25 20:36:39 -0500

    dao

---
+++ BookStore/src/main/java/dao/ShoesDAOImpl.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.ShoeType;
+
+public class ShoesDAOImpl implements ShoesDAO{
+
+	@Override
+	public ShoeType getAllShoes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
